91811eca2d7fa8a1a08d4f11289619efb3b13a3a
parth123965-wq <parth123965@gmail.com>
2026-09-01 18:25:46 +0530

    the otp-service data parse implements

--- backend/app/service/otp_service.py
+++ backend/app/service/otp_service.py
@@ -3,6 +3,7 @@ from secrets import randbelow
 from app.core.config import setting
 from fastapi import HTTPException, status
 import secrets
+import json
 
 class OtpService:
     
@@ -17,32 +18,37 @@ class OtpService:
     
     async def otp_cooldown(self, purpose: str, identifier: str)-> str:
             return f"otp_cooldown:{purpose}:{identifier.lower().strip()}"
+        
+    async def otp_data_key(self, purpose: str, identifier: str)-> str:
+        return f"otp_data:{purpose}:{identifier.lower().strip()}"
     
     async def otp_generate(self):
         return (randbelow(900000) + 100000)
     
-    async def save_otp(self, key: str, attempt: str, cooldown: str, otp_value: int) -> None:
+    async def save_otp(self, key: str, attempt: str, cooldown: str, otp_value: int, otp_data_key: str, otp_data:dict) -> None:
         async with self.redis.pipeline(transaction=True) as pipe:
             pipe.set(key,otp_value,ex=setting.OTP_EXPIRY)
             pipe.set(attempt,0,ex=setting.OTP_EXPIRY)
             pipe.set(cooldown,"1",ex=setting.OTP_EXPIRY)
+            pipe.set(otp_data_key,json.dumps(otp_data),ex=setting.OTP_EXPIRY)
             await pipe.execute()
             
-    async def otp_generate_save(self, purpose: str, identifier: str):
+    async def otp_generate_save(self, purpose: str, identifier: str, data: dict):
         otp_value = await self.otp_generate()
         key = await self.otp_key(purpose,identifier)
         attempt = await self.otp_attempt(purpose,identifier)
         cooldown = await self.otp_cooldown(purpose,identifier)
+        otp_data_key = await self.otp_data_key(purpose,identifier)
         if await self.redis.exists(cooldown):
             ttl = await self.redis.ttl(cooldown)
             raise HTTPException(
                 status_code=status.HTTP_429_TOO_MANY_REQUESTS,
                 detail=f"Please wait {ttl} seconds before requesting a new OTP."
             )
-        await self.save_otp(key=key,attempt=attempt,cooldown=cooldown,otp_value=otp_value)
+        await self.save_otp(key=key,attempt=attempt,cooldown=cooldown,otp_value=otp_value,otp_data_key=otp_data_key,otp_data=data)
         return otp_value
     
-    async def verify_otp(self, purpose: str, identifier: str, otp_value: int):
+    async def verify_otp(self, purpose: str, identifier: str, otp_value: int)->dict:
         otp_keys = await self.otp_key(purpose,identifier)
         attempt_key = await self.otp_attempt(purpose,identifier)
         attempt = await self.redis.incr(attempt_key)
@@ -60,5 +66,13 @@ class OtpService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"Invalid OTP. {remaining} attempt(s) remaining."
             )
-        await self.redis.delete(otp_keys,attempt_key,cooldown_key)
-        return True
+        otp_data_key = await self.otp_data_key(purpose,identifier)
+        cached = await self.redis.get(otp_data_key)
+        if not cached:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User Data not Found Please try Again."
+            )
+        data = json.loads(cached)
+        await self.redis.delete(otp_keys,attempt_key,cooldown_key,otp_data_key)
+        return data
